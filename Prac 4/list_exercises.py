d099d3b7fac85741eac776b56d9be5cc4b50335c
ChrisReppel <42366521+ChrisReppel@users.noreply.github.com>
2020-03-25 13:59:13 +1000

    completed basic security checker

--- Prac 4/list_exercises.py
+++ Prac 4/list_exercises.py
@@ -1,3 +1,4 @@
+# Basic List Operations
 numbers = []
 for i in range(5):
     number = int(input("Enter a number: "))
@@ -7,3 +8,12 @@ print("The smallest number is", min(numbers))
 print("The largest number is", max(numbers))
 print("The  sum of the numbers is", sum(numbers))
 print("The average of the numbers is", sum(numbers) / len(numbers))
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter your username: ")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
