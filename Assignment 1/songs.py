a0a1d70b159421d9c844a5ad0f80bbef66a8a22a
ChrisReppel <42366521+ChrisReppel@users.noreply.github.com>
2020-04-08 14:14:04 +1000

    Assignment menu and csv file

---
+++ Assignment 1/songs.py
@@ -0,0 +1,20 @@
+"""
+Chris Reppel
+Due Date: 9/4/20
+Songs to Learn 1.0
+"""
+
+MENU = """Menu:
+L - List songs
+A - Add a new Song
+C - Complete a Song
+Q - Quit"""
+
+import csv
+
+
+def main():
+    
+
+main()
+
